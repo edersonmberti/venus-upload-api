21c40a6eb56f41126998d92a8369ff53d2732238
Ederson Machado <edersonmberti@gmail.com>
2020-02-29 01:30:47 -0300

    update server file

--- src/server.js
+++ src/server.js
@@ -8,14 +8,14 @@ require("dotenv").config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
-app.use(cors());
-app.use(routes);
-
 app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
+
+app.use(routes);
 
 app.listen(process.env.PORT, () => {
   console.log("UHUL! The API is UP && RUNNING!!!", process.env.PORT);
